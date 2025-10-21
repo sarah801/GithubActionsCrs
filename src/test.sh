2b1c510579190c6754dbe239b4e6c0df9e78f270
Sarah801 <Sarah201758@miuegypt.edu.eg>
2025-10-21 14:45:00 +0300

    git creates a new NodeJS app

--- src/test.sh
+++ src/test.sh
@@ -1,9 +1,9 @@
-EXPECTED_OUTPUT="Hello, Test!"
+EXPECTED="Hello, Test!"
 OUTPUT=$(node -e "console.log(require('.\src\app.js')('Test'));")
-if [ "$OUTPUT" = "$EXPECTED_OUTPUT" ]; then
+if [ "$OUTPUT" = "$EXPECTED" ]; then
     echo "Test passed!"
     exit 0
 else
-    echo "Test failed! Expected '$EXPECTED_OUTPUT' but got '$OUTPUT'"
+    echo "Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
     exit 1
 fi
